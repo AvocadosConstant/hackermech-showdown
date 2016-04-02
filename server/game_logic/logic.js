1c4e1742875cd7fa905bcc3bc54d31a42ce98b17
Nik Vanderhof <nvander1@binghamton.edu>
2016-04-02 17:10:26 -0400

    Add game folder

---
+++ server/game_logic/logic.js
@@ -0,0 +1,22 @@
+var body_part = {
+  "torso": 250,
+  "arm": 100,
+  "leg": 100,
+}
+
+var starting_power = 100;
+
+function body_part(type) {
+  var new_part = {
+    "health" 
+  }
+}
+
+function player(player_id) {
+  this.player_id = player_id;
+  this.torso = body_part
+  this.left_arm = body_part("arm");
+  this.right_arm = body_part("arm");
+  this.left_leg = body_part("leg");
+  this.right_leg = body_part("leg");
+}
