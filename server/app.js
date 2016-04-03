12746e0b83810eb62925851ca7fcdbabcdec1f43
William Jagels <will.jagels@gmail.com>
2016-04-03 03:05:15 -0400

    Setup, join, clear

--- server/app.js
+++ server/app.js
@@ -1,12 +1,12 @@
+var debug = require('debug')('mech-fight-server:appjs');
+debug('launched app.js');
 var express = require('express');
 var bodyParser = require('body-parser');
-var debug = require('debug')('mech-fight-server:server');
 var morgan = require('morgan');
 
 var api = require('./routes/index');
 
 var app = express();
-var game = require('./game_logic/game.js');
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -18,20 +18,6 @@ app.use(morgan('dev'));
 
 app.use('/api', api);
 
-app.listen(process.env.PORT || 3000);
-
-app.get('/join', function (req, res) {
-  res.send(game.addPlayer());
-});
-
-app.get('/setup', function (req, res) {
-  var gameState = game.init();
-  res.send(JSON.stringify(gameState));
-});
-
-app.get('/submit', function (req, res) {
-  var player = req.body.player;
-  var code = req.body.code;
-  var result = game.run(player, code);
-  res.send(JSON.stringify(result));
+var listener = app.listen(process.env.PORT || 3000, function() {
+  debug('Listining on port %s.', listener.address().port);
 });
