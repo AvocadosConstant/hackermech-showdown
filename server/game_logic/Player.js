6f6a1715a44d9d9b2af89e48b1e0c1f6895fc40b
Nik Vanderhof <nvander1@binghamton.edu>
2016-04-02 23:45:53 -0400

    Move Player, BodyPart, and defaults into Player.js

--- server/game_logic/Player.js
+++ server/game_logic/Player.js
@@ -1,21 +1,30 @@
-var BodyPart = require('BodyPart.js');
-var defaults = require('defaults.js');
+/** Default values. */
+var defaults = {
+  'torso': 250,
+  'arm': 100,
+  'leg': 100,
+  'power': 100
+};
 
-/** A player. */
-class Player {
-  /**
-   * Creates a new player.
-   * @param {string} playerId - A unique identifier.
-   */
-  constructor(playerId) {
-    this.playerId = playerId;
-    this.power = defaults.power;
-    this.torso = new BodyPart('torso');
-    this.leftArm = new BodyPart('arm');
-    this.rightArm = new BodyPart('arm');
-    this.leftLeg = new BodyPart('leg');
-    this.rightLeg = new BodyPart('leg');
-  }
+/**
+ * Creates body part of a given type.
+ * @param {string} limbType - The type of body part.
+ */
+function BodyPart(limbType) {
+  this.limbType = limbType;
+  this.health = defaults[limbType];
 }
 
-module.exports = Player;
+/**
+ * Creates a new player.
+ * @param {string} playerId - A unique identifier.
+ */
+module.exports = function (playerId) {
+  this.playerId = playerId;
+  this.power = defaults.power;
+  this.torso = new BodyPart('torso');
+  this.leftArm = new BodyPart('arm');
+  this.rightArm = new BodyPart('arm');
+  this.leftLeg = new BodyPart('leg');
+  this.rightLeg = new BodyPart('leg');
+};
