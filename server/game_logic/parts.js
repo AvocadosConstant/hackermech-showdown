68754d08f8cbded9cecd3b6d9121a3f58d1fb03f
Andrew <andrewchellis@users.noreply.github.com>
2016-04-02 21:48:20 -0400

    Designed some modules and diversified the classes

--- server/game_logic/parts.js
+++ server/game_logic/parts.js
@@ -8,17 +8,31 @@
 * @param {boolean} pLeg - if the tool can be equipped on the leg
 * @param {number} cooldown - Turns that must be waited before reuse
 */
-function Part(pName,pDamage,pCost,pChest,pArm,pLeg,cooldown) {
-  return {
-    this.partID = pName;
-    this.damage = pDamage;
-    this.cost = pCost;
-    this.chest = pChest;
-    this.arm = pArm;
-    this.leg = pLeg;
+class Part {
+  constructor(partID,cost,chest,arm,leg,cooldown){
+    this.partID = name;
+    this.cost = cost;
+    this.chest = chest;
+    this.arm = arm;
+    this.leg = leg;
     this.cooldown = cooldown;
     this.cooling = 0;
-  };
+  }
+}
+
+class OffensivePart extends Part {
+  constructor(partID,damage,cost,chest,arm,leg,cooldown,type){
+    super(partID,cost,chest,arm,leg,cooldown);
+    this.damage = damage;
+  }
+}
+
+class DefensivePart extends Part {
+  constructor(partID,cost,chest,arm,leg,cooldown,utility){
+    super(partID,cost,chest,arm,leg,cooldown);
+    this.utility = utility;
+  }
+
 }
 
 /**
@@ -26,9 +40,13 @@ function Part(pName,pDamage,pCost,pChest,pArm,pLeg,cooldown) {
 */
 function makeParts(){
   var parts = [];
-  parts.push(new Part("rockets",20,15,true,true,true,8));
-  parts.push(new Part("flamethrower",15,12,false,true,false,4));
-  parts.push(new Part("machine gun",3,3,false,true,false,2));
-  parts.push(new Part("sword",5,3,false,true,false,0));
+  parts.push(new OffensivePart("rockets",20,15,true,true,true,8,"kinetic"));
+  parts.push(new OffensivePart("flamethrower",15,12,false,true,false,3,"thermal"));
+  parts.push(new OffensivePart("machine gun",3,3,false,true,false,1,"kinetic"));
+  parts.push(new OffensivePart("sword",5,3,false,true,false,0,"kinetic"));
+  parts.push(new OffensivePart("laser",15,17,false,true,false,4,"em"));
+  parts.push(new DefensivePart("shield",40,true,false,false,5,"block"));
+  //what if we make repair always available and have a module that enhances its efficiency
+  parts.push(new DefensivePart("repair",20,true,false,false,2,"heal 10 hp"));
   return parts;
 }
